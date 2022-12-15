f7a9e98843bf08ee1705f6ee3aec17574f119bf2
Alexander Ernst <alexander.ernst@student.ipso.ch>
2022-12-15 21:19:04 +0100

    kommentare

--- JetstreamServiceNET/Model/Order.cs
+++ JetstreamServiceNET/Model/Order.cs
@@ -1,12 +1,6 @@
 ﻿using JetstreamServiceNET.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace JetstreamServiceNET.Model
 {
